88ab3f91455964b1d451b07cab331383f1fafd54
yaeby <adrian.copta@isa.utm.md>
2024-11-20 11:54:07 +0200

    fxml

--- inventory-management-system/src/Main.java
+++ inventory-management-system/src/Main.java
@@ -1,9 +1,12 @@
 import commands.*;
+import gui.InventoryManagerApp;
 
 public class Main {
 
     public static void main(String[] args) {
-        CommandExecutor executor = new CommandExecutor();
-        executor.run();
+//        CommandExecutor executor = new CommandExecutor();
+//        executor.run();
+
+        InventoryManagerApp.main(args);
     }
 }
