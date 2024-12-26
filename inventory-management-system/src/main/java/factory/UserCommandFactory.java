f2bec6c753be0b1ec84373241627ba83910f5b55
yaeby <adrian.copta@isa.utm.md>
2024-12-26 13:27:08 +0200

    modiffs

--- inventory-management-system/src/main/java/factory/UserCommandFactory.java
+++ inventory-management-system/src/main/java/factory/UserCommandFactory.java
@@ -7,13 +7,6 @@ public class UserCommandFactory implements CommandFactory {
 
     @Override
     public Command createCommand(String commandType) {
-        return switch (commandType) {
-            case "1" -> new GetAllUsersCommand();
-            case "2" -> new GetUserByIdCommand();
-//            case "3" -> new AddUserCommand();
-//            case "4" -> new UpdateUserCommand();
-//            case "5" -> new DeleteUserCommand();
-            default -> null;
-        };
+        return null;
     }
 }
